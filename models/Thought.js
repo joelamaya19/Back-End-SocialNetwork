cd865651d950f0b8204f52fb5f94f4d6799671f8
joelamaya19 <joelamaya99@gmail.com>
2024-05-23 18:20:31 -0500

    fixed a minor bugs

--- models/Thought.js
+++ models/Thought.js
@@ -31,6 +31,6 @@ thoughtSchema.virtual('reactionCount').get(function() {
     return this.reactions.length;
 });
 
-const Thought = model('thought'. thoughtSchema);
+const Thought = model('thought', thoughtSchema);
 
 module.exports = Thought;
